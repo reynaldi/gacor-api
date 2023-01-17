fa12ba4d605879a74b8e0cfcab260605817686cb
Reynaldi Pranata Surya <reynaldi.surya@gmail.com>
2023-01-17 09:49:25 +0700

    feat: misc update

---
+++ tests/UserDom/UserRepo.cs
@@ -0,0 +1,15 @@
+namespace GacorApi.Tests.UserDom
+{
+    public class UserRepo
+    {
+        [Fact]
+        public async Task FindUserTest_ReturnNull()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
